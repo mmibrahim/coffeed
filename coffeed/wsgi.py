ebcc34cd9c94d988c51c11c3e96245c1b201c766
Mohamed Ibrahim <mmibrahim@hotmail.com>
2015-12-06 18:11:25 -0800

    Preparing for Heroku

--- coffeed/wsgi.py
+++ coffeed/wsgi.py
@@ -7,10 +7,17 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
+
 import os
+
+
 
 from django.core.wsgi import get_wsgi_application
 
+from dj_static import Cling
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffeed.settings")
 
-application = get_wsgi_application()
+
+application = Cling(get_wsgi_application())
